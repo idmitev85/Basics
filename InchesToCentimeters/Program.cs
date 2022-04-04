9d6ea478732fef68837d1566a66f39a60bb03629
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-04 19:30:59 +0300

    Add files via upload

---
+++ InchesToCentimeters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double INCH = 2.54;
+            double num = double.Parse(Console.ReadLine());
+            double centimetres = num * INCH;
+            Console.WriteLine(centimetres);
+        }
+    }
+}
---
+++ PetShop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double DOG = 2.50;
+            const double CAT = 4.00;
+            double num = double.Parse(Console.ReadLine());
+            double numb = double.Parse(Console.ReadLine());
+            double dogs  = num * DOG;
+            double cats = numb * CAT;
+            double result = dogs + cats;
+            Console.WriteLine($"{result} lv.");
+
+        }
+    }
+}
