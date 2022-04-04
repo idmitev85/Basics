d1c7935e67032f9e209bb6a799352c0dfbc542da
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-04 19:29:21 +0300

    Add files via upload

---
+++ YardGreening/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double YARD = 7.61;
+            const double OFF = 0.18;
+            double num = double.Parse(Console.ReadLine());
+            double all = num * YARD;
+            double priceoff = all * OFF;
+            double result = all - priceoff;
+            Console.WriteLine($"The final price is {result} lv.");
+            Console.WriteLine($"The discount is {priceoff} lv.");
+        }
+    }
+}
