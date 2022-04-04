671f833925a47739d3c9c924110d232450dda5cb
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-04 19:39:16 +0300

    Add files via upload

---
+++ Hallo Again/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hallo_Again
+{
+    class Program
+    {
+        public static int SoftUni { get; private set; }
+        public static bool Hallo { get; private set; }
+
+        static void Main(string[] args)
+        {
+            // how to strat the program ctrl + F5
+            Console.WriteLine(Hallo SoftUni!);
+        }
+    }
+}
