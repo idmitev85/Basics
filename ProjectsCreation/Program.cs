355c8dd393fc95d57e0a5e7cbbed3c18f378d304
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-04 19:30:10 +0300

    Add files via upload

---
+++ ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            const int PROJ = 3;
+            int number = int.Parse(Console.ReadLine());
+            int hours = number * PROJ;
+            Console.WriteLine($"The architect {name} will need {hours} hours to complete {number} project/s.");
+        }
+    }
+}
