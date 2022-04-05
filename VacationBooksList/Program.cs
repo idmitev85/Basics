d0a731f05e08dfd0146246e87bf3ec62bc742a77
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-05 19:06:41 +0300

    Add files via upload

---
+++ VacationBooksList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int all = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int result = ( all / count ) / days;
+            Console.WriteLine(result);
+        }
+    }
+}
