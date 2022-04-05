7217327466a2f2c979f11ab7ee9c39acd83185c5
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-05 19:09:21 +0300

    Add files via upload

---
+++ DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            double mounth = double.Parse(Console.ReadLine());
+            double profi = double.Parse(Console.ReadLine());
+            double result = money + mounth * (( money * ( profi / 100) )/12);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ FishTank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtCm = int.Parse(Console.ReadLine());
+            int widthCm = int.Parse(Console.ReadLine());
+            int hеightCm = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            int volumeAq = lenghtCm * widthCm * hеightCm;
+            double volumeLt = volumeAq / 1000.00;
+            double volumeNeed = volumeLt * (1 - percent / 100);
+            Console.WriteLine(volumeNeed);
+        }
+    }
+}
