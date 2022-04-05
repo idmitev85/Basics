7ca388d451bdf26406bb0d68b3add77c58b42046
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-05 19:10:07 +0300

    Add files via upload

---
+++ BasketballEquipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taxPerYear = int.Parse(Console.ReadLine());
+            double tranerPrice = 0.6 * taxPerYear;
+            double outfitPrice = 0.8 * tranerPrice;
+            double ballPrice = outfitPrice / 4;
+            double accPrice = ballPrice / 5;
+            double expenses = taxPerYear + tranerPrice + outfitPrice + ballPrice + accPrice;
+            Console.WriteLine(expenses);
+        }
+    }
+}
