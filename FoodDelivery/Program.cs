4b26bf65098486dca9cbd79bda1d43cf0438774f
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-05 19:08:22 +0300

    Add files via upload

---
+++ FoodDelivery/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken = int.Parse(Console.ReadLine());
+            int fish = int.Parse(Console.ReadLine());
+            int veg = int.Parse(Console.ReadLine());
+            double sum = chicken * 10.35 + fish * 12.40 + veg * 8.15;
+            double desertPrice = 0.20 * sum;
+            double allPrice = sum + desertPrice + 2.50;
+            Console.WriteLine(allPrice);
+        }
+    }
+}
