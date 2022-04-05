ecdf21ff176016bfec16c9b90575b0eea4bccf61
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-05 19:07:35 +0300

    Add files via upload

---
+++ Repainting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int plastic = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int thinner = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double plasticPrice = (plastic + 2) * 1.50;
+            double paintPrice = (paint + 0.1 * paint) * 14.50;
+            double thinnerPrice = thinner * 5;
+            double bagPrice = 0.4;
+            double expenses = plasticPrice + paintPrice + thinnerPrice + bagPrice;
+            double workPrice = (expenses * 0.3) * hours;
+            double allExpenses = expenses + workPrice;
+            Console.WriteLine(allExpenses);
+
+
+
+        }
+    }
+}
---
+++ SuppliesforSchool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SuppliesforSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pen = int.Parse(Console.ReadLine());
+            int mark = int.Parse(Console.ReadLine());
+            int clean = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+            double all = (pen * 5.80) + (mark * 7.20) + (clean * 1.20);
+            double disc = all * (discount / 100.0);
+            double result = all - disc;
+            Console.WriteLine(result);
+        }
+    }
+}
