7484db18c2842d6c29b63b3285bc799f1db7b105
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-06 20:52:29 +0300

    Add files via upload

---
+++ CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CircleAreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double circleRadius = double.Parse(Console.ReadLine());
+            double calculatedArea = Math.PI * (circleRadius * circleRadius);
+            double calculatedPerimeter = 2 * Math.PI * circleRadius;
+            Console.WriteLine($"{calculatedArea:f2}");
+            Console.WriteLine($"{calculatedPerimeter:f2}");
+        }
+    }
+}
