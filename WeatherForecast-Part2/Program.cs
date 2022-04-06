80ddcfa4d85952eb5b55b5c083b50177784e6096
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-06 20:47:01 +0300

    Add files via upload

---
+++ WeatherForecast-Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WeatherForecast_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degrees = double.Parse(Console.ReadLine());
+            if (degrees >= 26.00 && degrees <= 35.00)
+            {
+                Console.WriteLine("Hot");
+            }
+            else if (degrees > 20.00 && degrees < 26.00)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (degrees >= 15.00 && degrees <= 20.00)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if (degrees >= 12.00 && degrees < 15.00)
+            {
+                Console.WriteLine("Cool");
+            }
+            else if (degrees >= 5.00 && degrees < 12.00)
+            {
+                Console.WriteLine("Cold");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
