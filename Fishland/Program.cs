3b9b5e24fc887dc7b6eb9011630f9eee90bd87ab
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-06 20:51:37 +0300

    Add files via upload

---
+++ Fishland/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOfMackerel = double.Parse(Console.ReadLine());
+            double priceOfSprat = double.Parse(Console.ReadLine());
+            double quantityOfBonito = double.Parse(Console.ReadLine());
+            double quantityOfScad = double.Parse(Console.ReadLine());
+            int quantityOfMussels = int.Parse(Console.ReadLine());
+            const double priceOfMussels = 7.50;
+
+            double totalPriceOfBonito = priceOfMackerel * 1.60;
+            double totalPriceOfScad = priceOfSprat * 1.80;
+
+            double neededMoney = Math.Abs(totalPriceOfBonito * quantityOfBonito
+                + totalPriceOfScad * quantityOfScad
+                + priceOfMussels * quantityOfMussels);
+            Console.WriteLine($"{neededMoney:f2}");
+        }
+    }
+}
