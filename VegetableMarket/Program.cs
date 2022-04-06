c7fdef84fade748ff744c28a35bdccb09b7b45ce
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-06 20:48:23 +0300

    Add files via upload

---
+++ VegetableMarket/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priseForOneVegetables = double.Parse(Console.ReadLine());
+            double priseForOneFruit = double.Parse(Console.ReadLine());
+            int quantityOfVegetables = int.Parse(Console.ReadLine());
+            int quantityOfFruit = int.Parse(Console.ReadLine());
+            const double euro = 1.94;
+
+            double totalPrice = priseForOneVegetables * quantityOfVegetables + priseForOneFruit * quantityOfFruit;
+            double totalPriceInEuro = totalPrice / 1.94;
+            Console.WriteLine($"{totalPriceInEuro:f2}");
+        }
+    }
+}
---
+++ WeatherForecast/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weather = Console.ReadLine();
+            switch (weather)
+            {
+                case "sunny":
+                    Console.WriteLine("It's warm outside!");
+                    break;
+                default:
+                    Console.WriteLine("It's cold outside!");
+                    break;
+            }
+        }
+    }
+}
