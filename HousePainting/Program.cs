e3b31a72273bebe4de7c9c3c20afe0d44c786693
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-06 20:50:36 +0300

    Add files via upload

---
+++ HousePainting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HousePainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double heightOfHouse = double.Parse(Console.ReadLine());
+            double leghtOfHouseSide = double.Parse(Console.ReadLine());
+            double heightOfRoof = double.Parse(Console.ReadLine());
+
+            double areaOfDoor = 1.2 * 2;
+            double areaOfWindows = 2 * (1.5 * 1.5);
+            
+            const double areaOfGreenPaintPerLitre = 3.4;
+            const double areaOfRedPaintPerLitre = 4.3;
+            double totalAreaOfWalls = (leghtOfHouseSide * heightOfHouse) * 2 - areaOfWindows
+                + (heightOfHouse * heightOfHouse) * 2 - areaOfDoor;
+            double totalAreaOfRoof = (heightOfHouse * heightOfRoof / 2) * 2
+                + (heightOfHouse * leghtOfHouseSide) * 2;
+            double quantityOfGreenPaint = totalAreaOfWalls / areaOfGreenPaintPerLitre;
+            double quantityOfRedPaint = totalAreaOfRoof / areaOfRedPaintPerLitre;
+            Console.WriteLine($"{quantityOfGreenPaint:f2}");
+            Console.WriteLine($"{quantityOfRedPaint:f2}");
+        }
+    }
+}
---
+++ TrainingLab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double wightInMetеrs = double.Parse(Console.ReadLine());
+            double heightInMeters = double.Parse(Console.ReadLine());
+            const double heightRow = 70;
+            const double wightRow = 120;
+            double wightInCentimeters = wightInMetеrs * 100;
+            double heightInCentimeters = heightInMeters * 100 - 100;
+
+            double countOfRow = (heightInCentimeters - heightInCentimeters % heightRow) / heightRow;
+            double countOfColumn = (wightInCentimeters - wightInCentimeters % wightRow) / wightRow;
+            double totalSeats = countOfColumn * countOfRow - 3;
+            Console.WriteLine(totalSeats);
+        }
+    }
+}
