0e9341efe4504e326751f6d3195dc3144b4ca2bd
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-07 19:44:34 +0300

    Add files via upload

---
+++ GodzillaVsKong/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statistCount = int.Parse(Console.ReadLine());
+            double statistCoutPrisePerPerson = double.Parse(Console.ReadLine());
+
+            double statistPrise = statistCount * statistCoutPrisePerPerson;
+            double decorPrise = budget * 0.10;
+            if (statistCount > 150)
+            {
+                statistPrise *= 0.90;
+            }
+            double totalMoney = decorPrise + statistPrise;
+            if (totalMoney > budget)
+            {
+                double needMoney = totalMoney - budget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {needMoney:f2} leva more.");
+            }
+            else
+            {
+                double moneyLeft = budget - totalMoney;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {moneyLeft:f2} leva left.");
+            }
+        }
+    }
+}
