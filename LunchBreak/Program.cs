30995c1198c15564b40a7ec9ab5f80227971491d
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-07 19:43:32 +0300

    Add files via upload

---
+++ LunchBreak/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LunchBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tvSeries = Console.ReadLine();
+            int episodeTime = int.Parse(Console.ReadLine());
+            double brakeTime = double.Parse(Console.ReadLine());
+
+            double lunchTimeBrake = brakeTime / 8;
+            double relaxTime = brakeTime / 4;
+            double timeLeft = brakeTime - (lunchTimeBrake + relaxTime);
+            double needTime = Math.Abs(episodeTime - timeLeft);
+            Console.WriteLine(timeLeft >= episodeTime ? $"You have enough time to watch {tvSeries} and left with {Math.Ceiling(needTime)} minutes free time.": $"You don't have enough time to watch {tvSeries}, you need {Math.Ceiling(needTime)} more minutes." );
+        }
+    }
+}
---
+++ Shopping/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int numberOfVideoCards = int.Parse(Console.ReadLine());
+            int numberOfProcessors = int.Parse(Console.ReadLine());
+            int numberOfRamMemory = int.Parse(Console.ReadLine());
+
+            const double videoCardPrice = 250;
+            double totalVideoCardPrice = videoCardPrice * numberOfVideoCards;
+            double totalProcessorPrice = totalVideoCardPrice * 0.35;
+            double totalRamMemoryPrice = totalVideoCardPrice * 0.10;
+            
+            double totalMoney = totalVideoCardPrice + totalProcessorPrice * numberOfProcessors + totalRamMemoryPrice * numberOfRamMemory;
+            if (numberOfVideoCards > numberOfProcessors)
+            {
+                totalMoney *= 0.85;
+            }
+            double difference = Math.Abs(totalMoney - budget);
+            Console.WriteLine(budget >= totalMoney ?$"You have {difference:f2} leva left!" :$"Not enough money! You need {difference:f2} leva more!");
+        }
+    }
+}
