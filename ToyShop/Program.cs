fc962880d33a2757b5f6479891586ad5fc60d73b
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-07 19:40:39 +0300

    Add files via upload

---
+++ ToyShop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double puzzlePrise = 2.60;
+            const double dollPrise = 3.00;
+            const double bearPrise = 4.10;
+            const double minionPrise = 8.20;
+            const double truckPrise = 2.00;
+
+            double tripPrise = double.Parse(Console.ReadLine());
+            int puzzleCount = int.Parse(Console.ReadLine());
+            int dollCount = int.Parse(Console.ReadLine());
+            int bearCount = int.Parse(Console.ReadLine());
+            int minionCount = int.Parse(Console.ReadLine());
+            int truckCount = int.Parse(Console.ReadLine());
+            int toysCount = puzzleCount + dollCount + bearCount + minionCount + truckCount;
+            double toysPrise = puzzleCount * puzzlePrise + dollCount * dollPrise + bearCount * bearPrise + minionCount * minionPrise + truckCount * truckPrise;
+
+            if (toysCount >= 50)
+            {
+                toysPrise *= 0.75;
+                
+            }
+            double rent = toysPrise * 0.10;
+            double diference = Math.Abs(rent + tripPrise - toysPrise);
+            if (toysPrise >= rent + tripPrise)
+            {
+                Console.WriteLine($"Yes! {diference:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {diference:f2} lv needed.");
+            }
+        }
+    }
+}
---
+++ WorldSwimmingRecord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordSeconds = double.Parse(Console.ReadLine());
+            double distanceMetres = double.Parse(Console.ReadLine());
+            double timeSecundsForOneMeter = double.Parse(Console.ReadLine());
+
+            double neededTime =distanceMetres * timeSecundsForOneMeter;
+            double resistance = Math.Floor(distanceMetres / 15) * 12.5;
+            double totalTime = neededTime + resistance;
+            if (recordSeconds <= totalTime)
+            {
+                Console.WriteLine($"No, he failed! He was {(totalTime - recordSeconds):f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+        }
+    }
+}
