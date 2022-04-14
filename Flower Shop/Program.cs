8e858804bde91a2f834c216fe831ce03722390b5
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-14 15:36:24 +0300

    Add files via upload

---
+++ Flower Shop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Flower_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double priceOfMagnolias = 3.25;
+            const double priceOfHyacinths = 4;
+            const double priceOfRoses = 3.5;
+            const double priceOfCactuses = 8;
+
+            int countOfMagnolias = int.Parse(Console.ReadLine());
+            int countOfHyacinths = int.Parse(Console.ReadLine());
+            int countOfRoses = int.Parse(Console.ReadLine());
+            int countOfCactuses = int.Parse(Console.ReadLine());
+            double priceOfGift = double.Parse(Console.ReadLine());
+
+            double totalPriceOfFlowers = (countOfMagnolias * priceOfMagnolias + countOfHyacinths * priceOfHyacinths + countOfRoses * priceOfRoses + countOfCactuses * priceOfCactuses) * 0.95;
+            if (priceOfGift > totalPriceOfFlowers)
+            {
+                double neededMoney = Math.Ceiling(priceOfGift - totalPriceOfFlowers);
+                Console.WriteLine($"She will have to borrow {neededMoney} leva.");
+            }
+            else
+            {
+                double leftMoney = Math.Floor(totalPriceOfFlowers - priceOfGift);
+                Console.WriteLine($"She is left with {leftMoney} leva.");
+            }
+        }
+    }
+}
---
+++ Fuel Tank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Fuel_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfFuel = Console.ReadLine();
+            double litersInTank = double.Parse(Console.ReadLine());
+            if (typeOfFuel == "Diesel")
+            {
+                if (litersInTank >= 25)
+                {
+                    Console.WriteLine("You have enough diesel.");
+                }
+                else
+                {
+                    Console.WriteLine("Fill your tank with diesel!");
+                }
+            }
+            else if (typeOfFuel == "Gasoline")
+            {
+                if (litersInTank >= 25)
+                {
+                    Console.WriteLine("You have enough gasoline.");
+                }
+                else
+                {
+                    Console.WriteLine("Fill your tank with gasoline!");
+                }
+            }
+            else if (typeOfFuel == "Gas")
+            {
+                if (litersInTank >= 25)
+                {
+                    Console.WriteLine("You have enough gas.");
+                }
+                else
+                {
+                    Console.WriteLine("Fill your tank with gas!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+            
+        }
+    }
+}
