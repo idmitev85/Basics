1d1ff9eeeb77ff19540f01fe11e19649483e58e5
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-14 15:35:31 +0300

    Add files via upload

---
+++ Fuel Tank - Part 2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Fuel_Tank___Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double gasolinePricePerLiter = 2.22;
+            const double dieselPricePerLiter = 2.33;
+            const double gasPricePerLiter = 0.93;
+             
+            const double discountPerLiterGasoline = 0.18;
+            const double discountPerLiterDiesel = 0.12;
+            const double discountPerLiterGas = 0.08;           
+
+            string typeOfFuel = Console.ReadLine();
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            string clubCard = Console.ReadLine();
+            double priceOfLiterGasWithDiscount = 0;
+            double priceOfLiterGasolineWithDiscount = 0;
+            double priceOfLiterDieselWithDiscount = 0;            
+            double totalPriceOfFuel = 0;
+
+            if (typeOfFuel == "Gas")
+            {
+                if (clubCard == "Yes")
+                {
+                    priceOfLiterGasWithDiscount = gasPricePerLiter - discountPerLiterGas;
+                }
+                else
+                {
+                    priceOfLiterGasWithDiscount = gasPricePerLiter;
+                }
+                totalPriceOfFuel = fuelQuantity * priceOfLiterGasWithDiscount;
+            }
+            else if (typeOfFuel == "Gasoline")
+            {
+                if (clubCard == "Yes")
+                {
+                    priceOfLiterGasolineWithDiscount = gasolinePricePerLiter - discountPerLiterGasoline;
+                }
+                else
+                {
+                    priceOfLiterGasolineWithDiscount = gasolinePricePerLiter;
+                }
+                totalPriceOfFuel = fuelQuantity * priceOfLiterGasolineWithDiscount;
+            }
+            else if (typeOfFuel == "Diesel")
+            {
+                if (clubCard == "Yes")
+                {
+                    priceOfLiterDieselWithDiscount = dieselPricePerLiter - discountPerLiterDiesel;
+                }
+                else
+                {
+                    priceOfLiterDieselWithDiscount = dieselPricePerLiter;
+                }
+                totalPriceOfFuel = fuelQuantity * priceOfLiterDieselWithDiscount;
+            }            
+            if (fuelQuantity > 20 && fuelQuantity <= 25)
+            {
+                totalPriceOfFuel *= 0.92;                
+            }
+            else if (fuelQuantity > 25)
+            {
+                totalPriceOfFuel *= 0.90;                
+            }
+            Console.WriteLine($"{totalPriceOfFuel:f2} lv.");           
+        }
+    }
+}
---
+++ Harvest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double percentageForWine = 0.4;
+            const double neededGrapesPerLiter = 2.5;
+            
+            int squareMetersVineyard = int.Parse(Console.ReadLine());
+            double grapesForOneSquareMeter = double.Parse(Console.ReadLine());
+            int neededLitersOfWine = int.Parse(Console.ReadLine());
+            int countOfWorkers = int.Parse(Console.ReadLine());
+
+            double totalGrapes = squareMetersVineyard * grapesForOneSquareMeter;
+            double totalWine = totalGrapes * percentageForWine / neededGrapesPerLiter;
+            if (totalWine >= neededLitersOfWine)
+            {
+                double litersLeft = totalWine - neededLitersOfWine;
+                double litersPerWorker = litersLeft / countOfWorkers;
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(totalWine)} liters.");
+                Console.WriteLine($"{Math.Ceiling(litersLeft)} liters left -> {Math.Ceiling(litersPerWorker)} liters per person.");
+            }
+            else
+            {
+                double litersNeeded = neededLitersOfWine - totalWine;                
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(litersNeeded)} liters wine needed.");
+            }                
+        }
+    }
+}
