b5815a9f2f9831add127a8784d68bd1852b6cf52
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-14 15:34:12 +0300

    Add files via upload

---
+++ Pets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfDays = int.Parse(Console.ReadLine());
+            int foodLeftInKilograms = int.Parse(Console.ReadLine());
+            double foodPerDayForDogInKilograms = double.Parse(Console.ReadLine());
+            double foodPerDayForCatInKilograms = double.Parse(Console.ReadLine());
+            double foodPerDayForTurtleInGrams = double.Parse(Console.ReadLine());
+
+            double neededFoodForDog = countOfDays * foodPerDayForDogInKilograms;
+            double neededFoodForCat = countOfDays * foodPerDayForCatInKilograms;
+            double neededFoodForTurtle = countOfDays * (foodPerDayForTurtleInGrams / 1000);
+            double totalFoodNeeded = neededFoodForCat + neededFoodForDog + neededFoodForTurtle;
+            if (foodLeftInKilograms >= totalFoodNeeded)
+            {
+                double foodLeft = Math.Floor(foodLeftInKilograms - totalFoodNeeded);
+                Console.WriteLine($"{foodLeft} kilos of food left.");
+            }
+            else
+            {
+                double foodNeeded = Math.Ceiling(totalFoodNeeded - foodLeftInKilograms);
+                Console.WriteLine($"{foodNeeded} more kilos of food are needed.");
+            }
+        }
+    }
+}
---
+++ Pipes In Pool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pipes_In_Pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int volume = int.Parse(Console.ReadLine());
+            int firstPipeDebitPerHour = int.Parse(Console.ReadLine());
+            int secondPipeDebitPerHour = int.Parse(Console.ReadLine());
+            double hoursWorkerMising = double.Parse(Console.ReadLine());
+
+            double firstPipeQuantity = firstPipeDebitPerHour * hoursWorkerMising;
+            double secondPipeQuantity = secondPipeDebitPerHour * hoursWorkerMising;
+            double totalQuantityOfPipes = firstPipeQuantity + secondPipeQuantity;
+            if (totalQuantityOfPipes <= volume)
+            {
+                double percentOfPool = (totalQuantityOfPipes / volume) * 100;
+                double percentOfFirstPipe = (firstPipeQuantity / totalQuantityOfPipes) * 100;
+                double percentOfSeconrPipe = (secondPipeQuantity / totalQuantityOfPipes) * 100;
+                Console.WriteLine($"The pool is {percentOfPool:f2}% full. Pipe 1: {percentOfFirstPipe:f2}%. Pipe 2: {percentOfSeconrPipe:f2}%.");
+            }
+            else
+            {
+                double totalOverflows = totalQuantityOfPipes - volume;
+                Console.WriteLine($"For {hoursWorkerMising:f2} hours the pool overflows with {totalOverflows:f2} liters.");
+            }
+        }
+    }
+}
