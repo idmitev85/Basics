670a4a2f43cec8be471c1a9b691760b816fac771
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-14 15:32:55 +0300

    Add files via upload

---
+++ Sleepy Tom Cat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sleepy_Tom_Cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int minutesForPlayOnWork = 63;
+            const int minutesForPlayOnRest = 127;
+            const int neededMinutesPerYear = 30000;
+            const int daysOfYear = 365;
+            int countOfFreeDays = int.Parse(Console.ReadLine());
+
+            int minutesForPlayOnFreeDays = countOfFreeDays * minutesForPlayOnRest;
+            int minutesForPlayOnWorkDays = (daysOfYear - countOfFreeDays) * minutesForPlayOnWork;
+            int totalMinutesForPlayPerYear = minutesForPlayOnFreeDays + minutesForPlayOnWorkDays;
+            if (neededMinutesPerYear > totalMinutesForPlayPerYear)
+            {
+                int minutesLeft = neededMinutesPerYear - totalMinutesForPlayPerYear;
+                int hours = minutesLeft / 60;
+                int minutes = minutesLeft % 60;
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+            }
+            else if (neededMinutesPerYear < totalMinutesForPlayPerYear)
+            {
+                int minutesLeft = totalMinutesForPlayPerYear - neededMinutesPerYear;
+                int hours = minutesLeft / 60;
+                int minutes = minutesLeft % 60;
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{hours} hours and {minutes} minutes more for play");
+            }
+        }
+    }
+}
---
+++ TransportPrice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TransportPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double taxiStartPrice = 0.70;
+            const double taxiDayPricePerKilometer = 0.79;
+            const double taxiNightPricePerKilometer = 0.90;
+            const double busPricePerKilometer = 0.09;
+            const double trainPricePerKilometer = 0.06;
+
+            int kilometers = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            double totalPrice = 0;
+            if (kilometers < 20)
+            {
+                if (timeOfDay == "day")
+                {
+                    totalPrice = taxiStartPrice + (taxiDayPricePerKilometer * kilometers);
+                }
+                else if (timeOfDay == "night")
+                {
+                    totalPrice = taxiStartPrice + (taxiNightPricePerKilometer * kilometers);
+                }
+            }
+            else if (kilometers < 100)
+            {
+                totalPrice = busPricePerKilometer * kilometers;
+            }
+            else
+            {
+                totalPrice = trainPricePerKilometer * kilometers;
+            }
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
