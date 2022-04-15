6ef31eac1d654b29270ea555e94ed74b9fa0be59
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-15 10:20:00 +0300

    Add files via upload

---
+++ WorkingHours/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            if (hour >= 10 && hour <= 18 && dayOfWeek != "Sunday")
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
