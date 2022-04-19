02640aa8600bc8f4c7dfa1ecbc193bf886a79ef1
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-19 16:54:00 +0300

    Add files via upload

---
+++ SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbCount = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < numbCount; i++)
+            {
+                int numb = int.Parse(Console.ReadLine());
+                sum += numb;
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
