3cb0ad8a2597650a8be855322dbd1502fc45cc07
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-20 15:56:01 +0300

    Add files via upload

---
+++ NumberSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbCount = int.Parse(Console.ReadLine());
+            int maxNumb = int.MinValue;
+            int minNumb = int.MaxValue;
+            
+            
+            for (int i = 0; i < numbCount; i++)
+            {
+                int numb = int.Parse(Console.ReadLine());
+                if (numb > maxNumb)
+                {
+                    maxNumb = numb;
+                }
+                if (numb < minNumb)
+                {
+                    minNumb = numb;
+                }
+                
+            }
+            Console.WriteLine("Max number: " + maxNumb);
+            Console.WriteLine("Min number: " + minNumb);
+        }
+    }
+}
