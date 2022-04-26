1a44bbfbcc2053e2d31ce4096ad66fa627efd052
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-26 19:17:42 +0300

    Add files via upload

---
+++ CharacterSequence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int simbol = 0; simbol < input.Length; simbol++)
+            {
+                char letter = input[simbol];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
