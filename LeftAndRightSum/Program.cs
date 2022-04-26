9d4af8ede6db1ffa27ccb69b3cb8fa1f1dd1f677
Ivo Mitev <102940425+idmitev85@users.noreply.github.com>
2022-04-26 19:15:22 +0300

    Add files via upload

---
+++ LeftAndRightSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbCount = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < numbCount; i++)
+            {
+                int numb = int.Parse(Console.ReadLine());
+                sum1 += numb;
+            }
+            for (int i = 2; i < numbCount + 2; i++)
+            {
+                int numb = int.Parse(Console.ReadLine());
+                sum2 += numb;
+            }
+            if (sum1 == sum2)
+            {
+                Console.WriteLine("Yes, sum = " + sum1);
+            }
+            else if (sum1 != sum2)
+            {
+                int diff = Math.Abs(sum1 - sum2);
+                Console.WriteLine("No, diff = " + diff);
+            }
+
+        }
+    }
+}
